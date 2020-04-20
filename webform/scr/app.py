ae400cebb70d8a3bc51d474958bc8ee66f9cafbd
the21composer <42894877+the21composer@users.noreply.github.com>
2020-04-20 17:24:13 +0300

    Updated classes & links & added delete

--- webform/scr/app.py
+++ webform/scr/app.py
@@ -10,7 +10,7 @@ connect("mongodb://localhost:27017/myDatabase", alias="my-app")
 
 class Error(EmbeddedMongoModel):
     time = fields.DateTimeField()
-    errorID = fields.IntegerField()
+    etype = fields.CharField()
     description = fields.CharField()
     class Meta:
         write_concern = WriteConcern(j=True)
@@ -18,7 +18,7 @@ class Error(EmbeddedMongoModel):
     
 class Failure(EmbeddedMongoModel):
     time = fields.DateTimeField()
-    failureID = fields.IntegerField()
+    ftype = fields.CharField()
     description = fields.CharField()
     class Meta:
         write_concern = WriteConcern(j=True)
@@ -27,7 +27,7 @@ class Failure(EmbeddedMongoModel):
 class Repair(MongoModel):
     deadline = fields.DateTimeField()
     machineID = fields.IntegerField()
-    repairID = fields.IntegerField()
+    rtype = fields.CharField()
     description = fields.CharField()
     in_progress = fields.BooleanField()
     completed = fields.BooleanField()
@@ -55,12 +55,12 @@ def index():
 def worker():
     for repair in Repair.objects.all(): #iterator?
         repair.delete()
-    Repair(datetime.datetime.now(), 3, 5, "Small repair", False, False).save()
-    Repair(datetime.datetime.now(), 2, 4, "Wonderful repair", True, False).save()
-    Repair(datetime.datetime.now(), 1, 666, "Cute repair", True, False).save()
-    Repair(datetime.datetime.now(), 3, 333, "I wanna live", False, False).save()
-    Repair(datetime.datetime.now(), 2, 222, "I am tired of writing descriptions", False, True).save()
-    Repair(datetime.datetime.now(), 1, 123, "Ok?", False, True).save()
+    Repair(datetime.datetime.now(), 3, "rep_5", "Small repair", False, False).save()
+    Repair(datetime.datetime.now(), 2, "rep_4", "Wonderful repair", True, False).save()
+    Repair(datetime.datetime.now(), 1, "rep_777", "Cute repair", True, False).save()
+    Repair(datetime.datetime.now(), 3, "rep_333", "I wanna live", False, False).save()
+    Repair(datetime.datetime.now(), 2, "rep_222", "I am tired of writing descriptions", False, True).save()
+    Repair(datetime.datetime.now(), 1, "rep_123", "Ok?", False, True).save()
     return render_template('worker.html', Repair=Repair)
 
 @app.route('/site')
@@ -70,11 +70,11 @@ def site():
         eq.delete()
     eq1 = Equipment(machineID=1, model='MODEL1').save()
     eq2 = Equipment(machineID=2, model='MODEL5').save()
-    eq1.errors.append(Error(time=datetime.datetime.now(), errorID=1, description="ERROR1"))
-    eq1.errors.append(Error(time=datetime.datetime.now(), errorID=2, description="ERROR4"))
-    eq1.failures.append(Failure(time=datetime.datetime.now(), failureID=5, description="FAILURETYPE5"))
-    eq1.failures.append(Failure(time=datetime.datetime.now(), failureID=2, description="FAILURETYPE2"))
-    eq2.errors.append(Error(time=datetime.datetime.now(), errorID=112, description="MYSTERY"))
+    eq1.errors.append(Error(time=datetime.datetime.now(), etype="WRONG_INPUT", description="ERROR1"))
+    eq1.errors.append(Error(time=datetime.datetime.now(), etype="OUT_OF_TIME", description="ERROR4"))
+    eq1.failures.append(Failure(time=datetime.datetime.now(), ftype="OVERPOWER", description="FAILURETYPE5"))
+    eq1.failures.append(Failure(time=datetime.datetime.now(), ftype="POWER_LOSS", description="FAILURETYPE2"))
+    eq2.errors.append(Error(time=datetime.datetime.now(), etype="unknown", description="MYSTERY"))
     eq1.save()
     eq2.save()
     Equipment(machineID=3, model='NOERR').save()
@@ -89,3 +89,11 @@ def repair():
 @app.route('/foreman')
 def foreman():
     return render_template('foreman.html') 
+
+@app.route('/clear')
+def clear():
+    for eq in Equipment.objects.all(): # Deleting old data...
+        eq.delete()
+    for repair in Repair.objects.all(): #iterator?
+        repair.delete()
+    return 'Deleted!'
