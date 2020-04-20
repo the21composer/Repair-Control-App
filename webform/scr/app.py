ad17e6a0655aeafcf896d107b8537f3f97a117c8
the21composer <42894877+the21composer@users.noreply.github.com>
2020-04-20 15:07:05 +0300

    Added classes & UI worker table

--- webform/scr/app.py
+++ webform/scr/app.py
@@ -15,20 +15,32 @@ class Error(EmbeddedMongoModel):
     class Meta:
         write_concern = WriteConcern(j=True)
         connection_alias = 'my-app'
+    
+class Failure(EmbeddedMongoModel):
+    time = fields.DateTimeField()
+    failureID = fields.IntegerField()
+    description = fields.CharField()
+    class Meta:
+        write_concern = WriteConcern(j=True)
+        connection_alias = 'my-app'
 
-class Repair(EmbeddedMongoModel):
+class Repair(MongoModel):
     deadline = fields.DateTimeField()
     machineID = fields.IntegerField()
     repairID = fields.IntegerField()
     description = fields.CharField()
     in_progress = fields.BooleanField()
     completed = fields.BooleanField()
+    class Meta:
+        write_concern = WriteConcern(j=True)
+        connection_alias = 'my-app'
 
 class Equipment(MongoModel):
     machineID = fields.IntegerField()
     model = fields.CharField()
     revised_on = fields.DateTimeField()
     errors = fields.EmbeddedDocumentListField(Error)
+    failures = fields.EmbeddedDocumentListField(Failure)
     class Meta:
         write_concern = WriteConcern(j=True)
         connection_alias = 'my-app'
@@ -41,7 +53,15 @@ def index():
 
 @app.route('/worker')
 def worker():
-    return render_template('worker.html')
+    for repair in Repair.objects.all(): #iterator?
+        repair.delete()
+    Repair(datetime.datetime.now(), 3, 5, "Small repair", False, False).save()
+    Repair(datetime.datetime.now(), 2, 4, "Wonderful repair", True, False).save()
+    Repair(datetime.datetime.now(), 1, 666, "Cute repair", True, False).save()
+    Repair(datetime.datetime.now(), 3, 333, "I wanna live", False, False).save()
+    Repair(datetime.datetime.now(), 2, 222, "I am tired of writing descriptions", False, True).save()
+    Repair(datetime.datetime.now(), 1, 123, "Ok?", False, True).save()
+    return render_template('worker.html', Repair=Repair)
 
 @app.route('/site')
 def site():
@@ -52,6 +72,8 @@ def site():
     eq2 = Equipment(machineID=2, model='MODEL5').save()
     eq1.errors.append(Error(time=datetime.datetime.now(), errorID=1, description="ERROR1"))
     eq1.errors.append(Error(time=datetime.datetime.now(), errorID=2, description="ERROR4"))
+    eq1.failures.append(Failure(time=datetime.datetime.now(), failureID=5, description="FAILURETYPE5"))
+    eq1.failures.append(Failure(time=datetime.datetime.now(), failureID=2, description="FAILURETYPE2"))
     eq2.errors.append(Error(time=datetime.datetime.now(), errorID=112, description="MYSTERY"))
     eq1.save()
     eq2.save()
