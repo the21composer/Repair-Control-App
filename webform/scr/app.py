4436d61bbce4643f9dc8787b210eacf33dc7cf85
the21composer <42894877+the21composer@users.noreply.github.com>
2020-04-22 12:18:48 +0300

    Authorization & updated DB

--- webform/scr/app.py
+++ webform/scr/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, flash, render_template, request, redirect, session
 from bson.objectid import ObjectId
 from pymodm.connection import connect
 from pymongo.write_concern import WriteConcern
 from pymodm import EmbeddedMongoModel, MongoModel, fields
 import datetime
 import uuid
+import functools
 
 from app_models import User, Equipment, Error, Failure, Repair
 
@@ -18,7 +19,17 @@ def index():
     User('repair_manager', '1', 'repair_manager').save()
     User('site_manager', '1', 'site_manager').save()
     User('foreman', '1', 'foreman').save()
-    return render_template('index.html')
+    return render_template('index.html', login=session.get('user'))
+
+def logged_in(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if 'user' not in session:
+            return redirect('/login')
+        if session['objects'] != func.__name__:
+            return redirect('/')
+        return func(*args, **kwargs)
+    return wrapper
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -28,12 +39,14 @@ def login():
         try:
             user = User.objects.get({'_id': request.form['loginIn'], 'password': request.form['passwordIn']})
         except User.DoesNotExist:
-            return render_template('login.html', error='Bad email or password')
+            return render_template('login.html', error='Неверный логин или пароль')
         else:
             session['user'] = user.login
+            session['objects'] = user.role
         return redirect(user.role)
 
 @app.route('/worker', methods=['GET', 'POST'])
+@logged_in
 def worker():
     if request.method == 'GET':
         for repair in Repair.objects.all(): #iterator?
@@ -44,7 +57,7 @@ def worker():
         Repair(datetime.datetime.now(), 3, "rep_333", "I wanna live", False, False).save()
         Repair(datetime.datetime.now(), 2, "rep_222", "I am tired of writing descriptions", False, True).save()
         Repair(datetime.datetime.now(), 1, "rep_123", "Ok?", False, True).save()
-        return render_template('worker.html', Repair=Repair)
+        return render_template('worker.html', Repair=Repair, login=session['user'])
     else:
         repID = request.form.get('ID')
         repair = Repair.objects.get({'_id':  ObjectId(repID)})
@@ -56,10 +69,10 @@ def worker():
         else:
             repair.completed = True
         repair.save()
-        return render_template('worker.html', Repair=Repair)
-
+        return render_template('worker.html', Repair=Repair, login=session['user'])
 
 @app.route('/site')
+@logged_in
 def site():
     # Example of some data
     for eq in Equipment.objects.all(): # Deleting old data...
@@ -76,15 +89,17 @@ def site():
     Equipment(machineID=3, model='NOERR').save()
     Equipment(machineID=4, model='NOERR').save()
     # Created 2 Equipments with some errors and 2 Equipments without
-    return render_template('site_manager.html', Equipment=Equipment)
+    return render_template('site_manager.html', Equipment=Equipment, login=session['user'])
 
 @app.route('/repair')
+@logged_in
 def repair():
-    return render_template('repair_manager.html')
+    return render_template('repair_manager.html', login=session['user'])
 
 @app.route('/foreman')
+@logged_in
 def foreman():
-    return render_template('foreman.html') 
+    return render_template('foreman.html', login=session['user']) 
 
 @app.route('/clear')
 def clear():
@@ -93,3 +108,17 @@ def clear():
     for repair in Repair.objects.all(): #iterator?
         repair.delete()
     return 'Deleted!'
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    session.pop('objects', None)
+    flash('You have been successfully logged out.')
+    return redirect('/')
+
+def main():
+    app.run(debug=True)
+
+
+if __name__ == '__main__':
+    main()
